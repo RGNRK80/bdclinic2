370ee85db9a1c42a079ef53f694187fbe2fc5d9e
RGNRK80 <armhound@gmail.com>
2021-12-24 15:27:21 +0300

    ss

---
+++ src/main/java/by/vet/configuration/WebConfig.java
@@ -0,0 +1,29 @@
+package by.vet.configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan ("by.vet")
+@EnableWebMvc
+
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public DataSource dataSource(){
+        HikariConfig config=new HikariConfig("/db.properties");
+
+
+        System.out.println(config.getMinimumIdle());
+        return new HikariDataSource(config);
+    }
+
+
+}
