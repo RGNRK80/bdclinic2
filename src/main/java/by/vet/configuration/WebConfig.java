43f80806c638556e188f2c1c9679a7e361bb38f1
RGNRK80 <armhound@gmail.com>
2021-12-24 15:46:49 +0300

    ss

--- src/main/java/by/vet/configuration/WebConfig.java
+++ src/main/java/by/vet/configuration/WebConfig.java
@@ -5,6 +5,7 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
